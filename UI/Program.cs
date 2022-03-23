b6f66223e6f438137d380123501388b7b0a22e93
Cassandra McKellar <mckarca@gmail.com>
2022-03-23 00:06:52 -0600

    Finally set up?

---
+++ UI/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class UserInterface
+{
+    static public void Main()
+    {
+        Console.WriteLine("Dragon's Hoard");
+        Console.WriteLine("(1) Go Into Battle");
+        Console.WriteLine("(2) Visit Store");
+        Console.WriteLine("(3) View Character Information");
+        Console.WriteLine("(4) Read Instructions");
+        Console.WriteLine("(5) Return to Main Menu");
+    }
+
+}
+
