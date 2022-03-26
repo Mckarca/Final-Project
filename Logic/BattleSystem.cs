76821f56e74625a030b42094d18afbcd7262213e
Cassandra McKellar <mckarca@gmail.com>
2022-03-25 23:43:31 -0600

    Added a lot more to UI, lil bit more to Logic

---
+++ Logic/BattleSystem.cs
@@ -0,0 +1,81 @@
+﻿using System;
+namespace Battle
+{
+    interface IOpponents
+    {
+        int health();
+        int attackDamage();
+        string message();
+    }
+
+    public class LesserKnight : IOpponents
+    {
+        public virtual int health()
+        {
+            return 10;
+        }
+
+        public virtual int attackDamage()
+        {
+            return 2;
+        }
+
+        public virtual string message()
+        {
+            return "Get back scum!";
+        }
+    }
+
+    public class RegularKnight : LesserKnight, IOpponents
+    {
+        public override int health()
+        {
+            return 15;
+        }
+
+        public override int attackDamage()
+        {
+            return 4;
+        }
+
+        public override string message()
+        {
+            return "Prepare to die.";
+        }
+    }
+
+    public class GreaterKnight : RegularKnight, IOpponents
+    {
+        public override int health()
+        {
+            return 20;
+        }
+
+        public override int attackDamage()
+        {
+            return 6;
+        }
+
+        public override string message()
+        {
+            return "You will be slaughtered at my hands!";
+        }
+
+        public int superAttackDamage()
+        {
+            return 12;
+        }
+    }
+
+    public class UserCharacter
+    {
+        public int userHealth;
+        public int userAttackDamage;
+
+        public UserCharacter (int health, int attackDamage)
+        {
+            userHealth = health;
+            userAttackDamage = attackDamage;
+        }
+    }
+}
--- UI/UserInterface.cs
+++ UI/UserInterface.cs
@@ -1,10 +1,13 @@
 ﻿using System;
-using Backend;
+using Battle;
 
 public class UserInterface
 {
     static public void Main()
     {
+        Console.WriteLine($"\nHey there! This will be a game where you play as a dragon trying to rescue its precious gold from the corrupt king.");
+        Console.WriteLine("You'll have to fight your way past his various knights while you gain enough levels that you can finally defeat the evil king himself.");
+        Console.WriteLine($"For now though, I don't have a lot actually built, but you're welcome to explore what's here!\n");
         GameMenu();
     }
 
@@ -21,8 +24,17 @@ public class UserInterface
             Console.WriteLine("(3) View Character Information");
             Console.WriteLine("(4) Read Instructions");
             Console.WriteLine("(5) Return to Main Menu");
-            int userInput = Convert.ToInt32(Console.ReadLine());
-            GameMenuSelection(userInput);
+            int userInput;
+            try
+           {
+                userInput = Convert.ToInt32(Console.ReadLine());
+                GameMenuSelection(userInput);
+            }
+            catch
+            {
+                Console.WriteLine($"Sorry, that's not an authorized command! Please try again.\n");
+                GameMenu();
+            }
         }
 
     public static void GameMenuSelection(int userInput)
@@ -35,29 +47,73 @@ public class UserInterface
         {
             ReadInstructions();
         }
-        if (userInput != 1 && userInput != 4)
+        if (userInput == 2 || userInput == 3 || userInput == 5)
         {
-            Console.WriteLine($"Sorry! That option is not availible at the moment. Please choose something else! :)\n");
+            Console.WriteLine($"\nSorry! That option is not availible at the moment. Please choose something else! :)\n");
+            GameMenu();
+        }
+    
+        if (userInput != 1 & userInput != 2 & userInput != 3 & userInput != 4 & userInput != 5)
+        {
+            Console.WriteLine($"Sorry, that option doesn't exist! Please try again.\n");
             GameMenu();
         }
     }
 
     public static void GoIntoBattle()
     {
+        LesserKnight lesserKnight = new LesserKnight();
+        RegularKnight regularKnight = new RegularKnight();
+        GreaterKnight greaterKnight = new GreaterKnight();
+        UserCharacter Dragon = new UserCharacter(10, 2);
         Console.BackgroundColor = ConsoleColor.Red;
         Console.ForegroundColor = ConsoleColor.Black;
-        Console.WriteLine("BATTLE!");
+        Console.WriteLine($"/nBATTLE!");
         Console.ResetColor();
         Console.WriteLine("Time to fight!");
+        Console.WriteLine($"Dragon's health is {Dragon.userHealth}");
+        Console.WriteLine($"Lesser Knight's health is {lesserKnight.health()}");
+        Console.WriteLine($"Regular Knight's health is {regularKnight.health()}");
+        Console.WriteLine($"Greater Knight's health is {greaterKnight.health()}");
+        ExitProgramOption();
     }
-
     public static void ReadInstructions()
     {
         Console.BackgroundColor = ConsoleColor.DarkGray;
         Console.ForegroundColor = ConsoleColor.Black;
-        Console.WriteLine("Instructions:");
+        Console.WriteLine($"\nInstructions:");
         Console.ResetColor();
-        Console.WriteLine("Currently, you can only access this page of the game. New features are in development. I am currently focused on making the battle function work.");
+        Console.WriteLine("Currently, you can only access this page and the battle page of the game. New features are in development.");
+        Console.WriteLine("I am currently focused on making the battle function work. The Instructions page will be updated when there are actual functions that need instructions given.");
+        ExitProgramOption();
+    }
+
+    public static void ExitProgramOption()
+    {
+        int userInput = 0;
+        Console.WriteLine($"\n\nNow then, would you like to:");
+        Console.WriteLine("(1) Go back to the game menu");
+        Console.WriteLine("(2) Exit program");
+        try
+        {
+            userInput = Convert.ToInt32(Console.ReadLine());
+        }
+        catch
+        {}
+        if (userInput == 1)
+        {
+            Console.WriteLine();
+            GameMenu();
+        }
+        if (userInput == 2)
+        {
+            Console.WriteLine("Bye!");
+        }
+        if (userInput != 1 && userInput != 2)
+        {
+            Console.WriteLine("I'm sorry, that command was not recognized. Please try again.");
+            ExitProgramOption();
+        }
     }
 
 }
